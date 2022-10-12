47eb827a366e45e3cccb80947f160cf6998cf764
pot-code <136290354@qq.com>
2022-10-12 16:30:50 +0800

    fix: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,6 +13,5 @@ export default defineConfig({
         plugins: ["@emotion/babel-plugin"],
       },
     }),
-    tsconfigPaths(),
   ],
 });
