f94be2621cb61102fec2ef831fe76797e9f95885
pot-code <136290354@qq.com>
2022-11-01 14:40:10 +0800

    feat: storybook config files

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
-import React from "react";
-import { useToggle } from "react-use";
 import { useTranslation } from "react-i18next";
 
 function App() {
-  const [show, toggle] = useToggle(false);
   const { t } = useTranslation();
 
-  return (
-    <div>
-      {show && <h1>{t("hello")}</h1>}
-      <button type="button" onClick={toggle}>
-        {t("start")}
-      </button>
-    </div>
-  );
+  return <h1>{t("hello")}</h1>;
 }
 
 export default App;
