b3db0c773c4631c7516a2d014b419b0ff555b3ff
pot-code <136290354@qq.com>
2022-11-01 14:43:52 +0800

    feat: vite proxy api calls

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,11 @@ export default defineConfig({
   build: {
     outDir: "build",
   },
+  server: {
+    proxy: {
+      "/api": "http://127.0.0.1:8000",
+    },
+  },
   plugins: [
     react({
       jsxImportSource: "@emotion/react",
