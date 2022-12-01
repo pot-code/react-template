ed5fe780e8bb1dbaa1368ec2a278356f72de5ed5
pot-code <136290354@qq.com>
2022-12-01 18:02:18 +0800

    refactor: remove @testing-library/react-hooks

--- src/hooks/usePaginatedSearch.test.ts
+++ src/hooks/usePaginatedSearch.test.ts
@@ -1,4 +1,4 @@
-import { act, renderHook } from "@testing-library/react-hooks";
+import { act, renderHook } from "@testing-library/react";
 import { OptionalPaginationParams } from "../types/pagination";
 import usePaginatedSearch from "./usePaginatedSearch";
 
--- src/hooks/useSearch.test.ts
+++ src/hooks/useSearch.test.ts
@@ -1,4 +1,4 @@
-import { act, renderHook } from "@testing-library/react-hooks";
+import { act, renderHook } from "@testing-library/react";
 import useSearch from "./useSearch";
 
 test("empty init params", () => {
