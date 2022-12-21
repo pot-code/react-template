8cd16c6105ffeb20cdef2c675fd20fc72ff058f4
pot-code <136290354@qq.com>
2022-12-21 11:43:37 +0800

    fix: ambient types

---
+++ src/types/emotion.d.ts
@@ -0,0 +1,5 @@
+import type { Theme as AppTheme } from "../theme";
+
+declare module "@emotion/react" {
+  export interface Theme extends AppTheme {}
+}
--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,14 +1,10 @@
 /// <reference types="vite/client" />
-import type { Theme as AppTheme } from "./theme";
 
 interface ImportMetaEnv {
   readonly VITE_ENDPOINT: string;
+  readonly VITE_PROJECT_NAME: string;
 }
 
 interface ImportMeta {
   readonly env: ImportMetaEnv;
-}
-
-declare module "@emotion/react" {
-  export interface Theme extends AppTheme {}
 }
