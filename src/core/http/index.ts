61c1b9c82a85667c687331a9f49efd9bdc3323fc
pot-code <136290354@qq.com>
2023-08-25 14:55:14 +0800

    feat: HttpClient class

--- src/core/http/index.ts
+++ src/core/http/index.ts
@@ -1,17 +1,48 @@
-import axios from "axios"
+import axios, { AxiosInstance, AxiosRequestConfig } from "axios"
 import { captureBusinessError, handleRejection } from "./interceptors"
 
-const http = axios.create({
+const instance = axios.create({
   baseURL: import.meta.env.VITE_API_PREFIX,
 })
 
-http.interceptors.request.use((config) => {
+instance.interceptors.request.use((config) => {
   config.headers.setAuthorization(`Bearer ${import.meta.env.VITE_API_TOKEN}`)
   return config
 })
-http.interceptors.response.use(captureBusinessError, handleRejection)
+instance.interceptors.response.use(captureBusinessError, handleRejection)
 
-export default http
+class HttpClient {
+  constructor(private readonly client: AxiosInstance) {}
+
+  async get<T>(url: string, config?: AxiosRequestConfig<any>) {
+    return this.client.get<T>(url, config).then((res) => res.data)
+  }
+
+  async post<T>(url: string, data: any, config?: AxiosRequestConfig<any>) {
+    return this.client.post<T>(url, data, config).then((res) => res.data)
+  }
+
+  async put<T>(url: string, data: any, config?: AxiosRequestConfig<any>) {
+    return this.client.put<T>(url, data, config).then((res) => res.data)
+  }
+
+  async delete<T>(url: string, config?: AxiosRequestConfig<any>) {
+    return this.client.delete<T>(url, config).then((res) => res.data)
+  }
+
+  async method(method: string, url: string, data: any, config?: AxiosRequestConfig<any>) {
+    return this.client
+      .request({
+        method,
+        url,
+        data,
+        ...config,
+      })
+      .then((res) => res.data)
+  }
+}
+
+export default new HttpClient(instance)
 
 export type { HttpResponse } from "./types"
 export { HttpError } from "./error"
--- src/views/home.tsx
+++ src/views/home.tsx
@@ -1,11 +1,11 @@
-import { useTranslation } from "react-i18next"
 import { useQuery } from "@tanstack/react-query"
-import DemoComp from "@/components/demo-comp"
+import { useTranslation } from "react-i18next"
 import { demoApi } from "@/api/demo"
+import DemoComp from "@/components/demo-comp"
 
 export default function HomeView() {
   const { t } = useTranslation()
-  const { data, isLoading } = useQuery(["hello"], ({ signal }) => demoApi.hello(signal).then((v) => v.data))
+  const { data, isLoading } = useQuery(["hello"], ({ signal }) => demoApi.hello(signal))
 
   if (isLoading) return <h1 className="text-gray-5">loading</h1>
 
