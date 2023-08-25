ba0cba7e56c5e2868053c7f3c131037bbae7d99e
pot-code <136290354@qq.com>
2023-08-25 15:33:32 +0800

    feat: HttpClient 类请求方法泛型参数

--- src/core/http/index.ts
+++ src/core/http/index.ts
@@ -5,28 +5,24 @@ const instance = axios.create({
   baseURL: import.meta.env.VITE_API_PREFIX,
 })
 
-instance.interceptors.request.use((config) => {
-  config.headers.setAuthorization(`Bearer ${import.meta.env.VITE_API_TOKEN}`)
-  return config
-})
 instance.interceptors.response.use(captureBusinessError, handleRejection)
 
 class HttpClient {
   constructor(private readonly client: AxiosInstance) {}
 
-  async get<T>(url: string, config?: AxiosRequestConfig<any>) {
+  async get<T = any>(url: string, config?: AxiosRequestConfig<any>) {
     return this.client.get<T>(url, config).then((res) => res.data)
   }
 
-  async post<T>(url: string, data: any, config?: AxiosRequestConfig<any>) {
+  async post<T = any>(url: string, data: any, config?: AxiosRequestConfig<any>) {
     return this.client.post<T>(url, data, config).then((res) => res.data)
   }
 
-  async put<T>(url: string, data: any, config?: AxiosRequestConfig<any>) {
+  async put<T = any>(url: string, data: any, config?: AxiosRequestConfig<any>) {
     return this.client.put<T>(url, data, config).then((res) => res.data)
   }
 
-  async delete<T>(url: string, config?: AxiosRequestConfig<any>) {
+  async delete<T = any>(url: string, config?: AxiosRequestConfig<any>) {
     return this.client.delete<T>(url, config).then((res) => res.data)
   }
 
--- src/views/home.tsx
+++ src/views/home.tsx
@@ -5,11 +5,11 @@ import DemoComp from "@/components/demo-comp"
 
 export default function HomeView() {
   const { t } = useTranslation()
-  const { data, isLoading } = useQuery(["hello"], ({ signal }) => demoApi.hello(signal))
+  const { data, isLoading } = useQuery(["hello"], ({ signal }) => demoApi.hello(signal).then((res) => res.data))
 
   if (isLoading) return <h1 className="text-gray-5">loading</h1>
 
-  if (data?.data) return <DemoComp title={t(data.data)} />
+  if (data) return <DemoComp title={t(data)} />
 
   return null
 }
