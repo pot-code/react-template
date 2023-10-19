444fb32748869f4d42694ad11602ff31657558e4
pot-code <136290354@qq.com>
2023-10-19 17:39:36 +0800

    feat: env variables

--- mock/handlers/hello.ts
+++ mock/handlers/hello.ts
@@ -1,7 +1,7 @@
 import { rest } from "msw"
 
 export const handlers = [
-  rest.get("/api/hello", (_, res, ctx) =>
+  rest.get("/mock/hello", (_, res, ctx) =>
     res(
       ctx.delay(500),
       ctx.json({
