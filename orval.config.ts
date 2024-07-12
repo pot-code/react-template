0c960a40911756a23392a5c696d3d353a7f9d364
pot-code <136290354@qq.com>
2024-07-12 11:40:14 +0800

    refactor: openapi generation

--- orval.config.ts
+++ orval.config.ts
@@ -1,8 +1,31 @@
 import { defineConfig } from "orval"
 
-export default defineConfig({
-  flow: {
-    input: "openapi/demo.yaml",
-    output: "src/api/demo.ts",
-  },
-})
+const modules = ["demo"]
+
+export default defineConfig(
+  Object.fromEntries(
+    modules.map((name) => [
+      name,
+      {
+        input: {
+          target: "http://127.0.0.1:8082/openapi.json",
+          filters: {
+            tags: [name],
+          },
+        },
+        output: {
+          prettier: true,
+          schemas: `./src/api/model`,
+          target: `./src/api/${name}.ts`,
+          override: {
+            mutator: {
+              path: "src/lib/http/instance.ts",
+              name: "customInstance",
+            },
+            operationName: ({ operationId }) => operationId!.split("#")[1],
+          },
+        },
+      },
+    ]),
+  ),
+)
